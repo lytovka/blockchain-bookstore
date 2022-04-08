113581e6d25dfd877d948a6c35f9573606f89b01
Ivan Lytovka <ivanlytovka@gmail.com>
2022-04-08 14:55:44 -0400

    init dapp

---
+++ dapp/configs/config.ts
@@ -0,0 +1,74 @@
+import type { AbiItem } from "web3-utils";
+export const CONTACT_ADDRESS = "0xeE7ed279EFBbF32CC8f78e3c734713afC1D215f2";
+
+export const CONTACT_ABI: Array<AbiItem> = [
+  {
+    constant: true,
+    name: "count",
+    inputs: [],
+    outputs: [
+      {
+        name: "",
+        type: "uint256",
+      },
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+    // signature: "0x06661abd",
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "contacts",
+    outputs: [
+      {
+        name: "id",
+        type: "uint256",
+      },
+      {
+        name: "name",
+        type: "string",
+      },
+      {
+        name: "phone",
+        type: "string",
+      },
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+    // signature: "0xe0f478cb",
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor",
+    // signature: "constructor",
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        name: "_name",
+        type: "string",
+      },
+      {
+        name: "_phone",
+        type: "string",
+      },
+    ],
+    name: "createContact",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+    // signature: "0x3dce4920",
+  },
+];
