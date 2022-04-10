8dfadd91023a59268867b97093871f3a8fb1fa67
Ivan Lytovka <ivanlytovka@gmail.com>
2022-04-10 19:46:11 -0400

    add navbar (unstyled)

---
+++ dapp/src/components/navbar/styled.ts
@@ -0,0 +1,19 @@
+import { AppBar, Box, styled, Toolbar } from "@mui/material";
+
+export const CustomAppBar = styled(AppBar)((props) => ({
+  backgroundColor: props.theme.palette.grey[800],
+}));
+
+export const CustomToolbar = styled(Toolbar)((props) => ({
+  maxWidth: props.theme.breakpoints.values["lg"],
+  width: "100%",
+  padding: "0",
+  justifyContent: "space-between",
+  margin: "0 auto",
+}));
+
+export const MenuItems = styled(Box)({
+  overflowX: "scroll",
+  display: "flex",
+  gap: 16,
+});
