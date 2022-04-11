13e5b259144fd08b2e4cd516f2e762fa2f23ed7a
Ivan Lytovka <ivanlytovka@gmail.com>
2022-04-11 19:01:52 -0400

    extend babel config

---
+++ dapp/babel.config.js
@@ -0,0 +1,16 @@
+module.exports =
+{
+    presets: ["next/babel"],
+    plugins: [
+        [
+            'module-resolver',
+            {
+                alias: {
+                    '~/components': './src/components',
+                    '~/contexts': './src/contexts',
+                    '~/theme': './src/theme',
+                },
+            },
+        ],
+    ]
+}
--- dapp/src/pages/signin/index.tsx
+++ dapp/src/pages/signin/index.tsx
@@ -1,38 +1,28 @@
-import { useState } from "react";
-import Web3 from "web3";
 import { Typography } from "@mui/material";
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Navbar } from "../../components";
+import { PageLayout } from "~/components";
+import { useWeb3Context } from "~/contexts/Web3Context";
 
 const Home: NextPage = () => {
-  const [account, setAccount] = useState(""); // state variable to set account.
-
-  // TODO: move Web3 logic to global-scope context
-  const handleSignIn = async () => {
-    const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
-    const accounts = await web3.eth.requestAccounts();
-    console.log({ accounts });
-    setAccount(accounts[0]);
-  };
-
+  const { handleSignIn, account } = useWeb3Context();
   return (
     <div>
       <Head>
         <title>Blockchain Bookstore Sign in</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar />
-      <main>
+      <PageLayout>
         {account ? (
           <>
-            <Typography variant="h1">Your account is: {account}</Typography>
+            <Typography fontSize="2rem" variant="h1">
+              Your account is: {account}
+            </Typography>
           </>
         ) : (
           <button onClick={handleSignIn}>Sign in</button>
         )}
-      </main>
-
+      </PageLayout>
       <footer></footer>
     </div>
   );
